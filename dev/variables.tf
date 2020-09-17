194ea340ec21fe40573d8ae9213460c3f84e0063
kyledakid <nvietthu@gmail.com>
2020-09-17 22:59:27 +0700

    Fix

---
+++ dev/variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "env" {
+  type = map
+}
+
+variable "project" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
