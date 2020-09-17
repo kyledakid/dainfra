e1a0f4627a07b99c0d6a47082153643be340af4f
kyledakid <nvietthu@gmail.com>
2020-09-17 22:56:04 +0700

    Add VPC for DEV

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,33 @@
 terraform {
-    required_version = ">= 0.13.0"
+  required_version = ">= 0.13.0"
 
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "elyk"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "elyk"
 
-        workspaces {
-          prefix = "dainfra-dev"
-        }
+    workspaces {
+      prefix = "dainfra-dev"
     }
+  }
 }
 
-provider "aws" {}
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name           = var.project
+  cidr           = var.cidr
+  azs            = var.azs
+  public_subnets = var.public_subnets
+
+  tags = {
+    Owner       = var.project
+    Environment = var.env[terraform.workspace]
+  }
+  vpc_tags = {
+    Name = var.project
+  }
+}
---
+++ dev/terraform.auto.tfvars
@@ -0,0 +1,13 @@
+project = "dainfra"
+region  = "us-east-1"
+azs = [
+  "us-east-1a",
+  "us-east-1b",
+  "us-east-1c"
+]
+cidr = "172.16.0.0/16"
+public_subnets = [
+  "172.16.1.0/24",
+  "172.16.2.0/24",
+  "172.16.3.0/24",
+]
