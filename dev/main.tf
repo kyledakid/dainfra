f2874780a3dc3db3679e6ff2b85525fb5a9eef15
Kyle Nguyen <nvietthu@gmail.com>
2020-09-26 17:21:37 +0700

    Test TFC (#4)

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,5 @@
 terraform {
   required_version = ">= 0.13.0"
-
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "elyk"
-
-    workspaces {
-      name = "dainfra-dev"
-    }
-  }
 }
 
 provider "aws" {
